debdc4be3e8a6cdb3aff76e338ed6a9ba149b1ff
kukab-shakil <49521206+kukab-shakil@users.noreply.github.com>
2019-05-30 04:47:52 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+/*
+   function names can NOT have:
+   - spaces
+   - any special characters except _
+   function names CAN have:
+   - letters
+   - numbers
+   function naming convention is: firstSecondThirdFourth (called Camel Case)
+*/
+function showAlert() {
+   console.log('Alert # 1');
+   console.log('Alert # 2');
+   console.log('Alert # 3');
+   console.log('Alert # 4');
+}
+
+function secondFunc() {
+   alert('Sarah Ahmed');
+}
+function changeName(){
+ console.log(document.querySelector('h1 span').innerHTML=document.querySelector('input').value);
+}
+function changeColor(){
+   document.querySelector('h1').style="color:blue";
+}
+function clearColor(){
+   document.querySelector('h1').style="color:black";
+}
+// Single line comments
+// phly hm code nhe pseudocode likhyn gy
+
+
+/*
+show userinput inside
+get input element on which usr typed some thing
+get value of input
+get heading element
+change value of heading and content
+how to get attribute
+This
+is a
+multi-line comment
+
+*/
+
+// An English paragraph has many SENTENCES. Each sentence ends with a FULL-STOP
+// A Javascript code has many STATEMENTS. Each statement ends with a SEMI-COLON.
+
+// Each JS statement is written on a separate line.
